6f854a84552e9f7ca7c12a8219e2ab96dba3bfc3
Fluke <dr.catth@gmail.com>
2023-12-01 15:02:25 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
+    int num;
+    int evenCount = 0;
+    int oddCount = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num;
+
+    while (num != 0) {
+        if (num%2 == 0) {
+            evenCount++;
+        }else{
+            oddCount++;
+        }
+        cout << "Enter an integer: ";
+        cin >> num;
+    }
+
+    cout << "#Even numbers = " << evenCount << endl;
+    cout << "#Odd numbers = " << oddCount << endl;
     return 0;
 }
