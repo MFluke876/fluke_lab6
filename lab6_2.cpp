beb398e3c445993631f48afde64ea2467172cb69
Fluke <dr.catth@gmail.com>
2023-12-04 12:57:56 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+double deg2rad(double deg){
+    return deg * M_PI / 180;
+}
+
+double rad2deg(double rad){
+    return rad * 180 / M_PI;
+}
+
+double findXComponent(double len1, double len2, double rad1, double rad2){
+    double x1;
+    double x2;;
+
+    x1 = len1 * cos(rad1);
+    x2 = len2 * cos(rad2);
+
+    return x1 + x2;
+}
+
+double findYComponent(double len1, double len2, double rad1, double rad2){
+    double y1;
+    double y2;;
+
+    y1 = len1 * sin(rad1);
+    y2 = len2 * sin(rad2);
+
+    return y1 + y2; 
+}
+
+double pythagoras(double xcomp, double ycomp){
+    return sqrt(pow(xcomp, 2) + pow(ycomp, 2));
+}
+
+void showResult(double vecLen, double vecDir){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+    cout << "Length of the resultant vector = " << vecLen << endl;
+    cout << "Direction of the resultant vector (deg) = " << vecDir << endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
